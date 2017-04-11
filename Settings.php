2f1962ac512eb917f09ad0a0b5abb21ed55132b4
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-12 00:59:10 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -2,4 +2,20 @@
 namespace Dfe\Robokassa;
 // 2017-04-10
 /** @method static Settings s() */
-final class Settings extends \Df\Payment\Settings {}
+final class Settings extends \Df\Payment\Settings {
+	/**
+	 * 2017-04-12
+	 * «Password #1»
+	 * [Robokassa] Where are my API credentials and settings located? https://mage2.pro/t/3667
+	 * @return string
+	 */
+	function password1() {return $this->testableP();}
+
+	/**
+	 * 2017-04-12
+	 * «Password #2»
+	 * [Robokassa] Where are my API credentials and settings located? https://mage2.pro/t/3667
+	 * @return string
+	 */
+	function password2() {return $this->testableP();}
+}
