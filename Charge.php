a8989fe7cbebe388256a5693fc22d8fe3a425933
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-16 05:31:55 +0300

    0.1.1

--- Charge.php
+++ Charge.php
@@ -2,6 +2,9 @@
 namespace Dfe\Robokassa;
 /**
  * 2017-04-10
+ * 2017-04-16
+ * «Description of variables, parameters and values»: http://docs.robokassa.ru/en/#2501
+ * «Описание переменных, параметров и значений»: http://docs.robokassa.ru/#1061
  * @method Method m()
  * @method Settings s()
  */
@@ -13,7 +16,12 @@ final class Charge extends \Df\PaypalClone\Charge {
 	 * @used-by \Df\PaypalClone\Charge::p()
 	 * @return array(string => mixed)
 	 */
-	protected function pCharge() {$s = $this->s(); return [];}
+	protected function pCharge() {$s = $this->s(); return [
+		// 2017-04-16
+		// «Means the Shop Identifier in ROBOKASSA you specified upon creation of the Shop.»
+		// «Идентификатор магазина в ROBOKASSA, который Вы придумали при создании магазина.»
+		'MerchantLogin' => $s->merchantID()
+	];}
 
 	/**
 	 * 2017-04-10
