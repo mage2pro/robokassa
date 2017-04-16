f1fc05402d5905b1c58eabdfb917d3bca257a563
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-16 05:48:21 +0300

    no message

--- Charge.php
+++ Charge.php
@@ -19,8 +19,29 @@ final class Charge extends \Df\PaypalClone\Charge {
 	protected function pCharge() {$s = $this->s(); return [
 		// 2017-04-16
 		// «Means the Shop Identifier in ROBOKASSA you specified upon creation of the Shop.»
+		// http://docs.robokassa.ru/en/#2503
 		// «Идентификатор магазина в ROBOKASSA, который Вы придумали при создании магазина.»
+		// http://docs.robokassa.ru/en/#1068
+		// Required.
 		'MerchantLogin' => $s->merchantID()
+		//
+		// 2017-04-16
+		// «Means the amount payable (in other words, the price of the order placed by the client).
+		// The format of presentation – dot-delimited digits. For example 123.45.
+		// The amount should be denominated in RUB.
+		// However, if the prices are denominated (e.g.) in USD on your website
+		// when issuing the invoice you need to specify the amount converted from USD to RUB.
+		// (see Optional Parameters OutSumCurrency).»
+		// http://docs.robokassa.ru/en/#2504
+		// «Требуемая к получению сумма (буквально — стоимость заказа, сделанного клиентом).
+		// Формат представления — число, разделитель — точка, например: 123.45.
+		// Сумма должна быть указана в рублях.
+		// Но, если стоимость товаров у Вас на сайте указана, например, в долларах,
+		// то при выставлении счёта к оплате Вам необходимо указывать уже пересчитанную сумму
+		// из долларов в рубли. См. необязательный параметр OutSumCurrency.»
+		// http://docs.robokassa.ru/#1188
+		// Required.
+		,'OutSum' => $this->amountF()
 	];}
 
 	/**
--- Method.php
+++ Method.php
@@ -4,10 +4,38 @@ namespace Dfe\Robokassa;
 final class Method extends \Df\PaypalClone\Method {
 	/**
 	 * 2017-04-16
+	 * «The format of presentation – dot-delimited digits. For example 123.45.»
+	 * http://docs.robokassa.ru/en/#2504
+	 * «Формат представления — число, разделитель — точка, например: 123.45.»
+	 * http://docs.robokassa.ru/#1188
+	 * @override
+	 * @see \Df\Payment\Method::amountFormat()
+	 * @used-by \Df\Payment\Operation::amountFormat()
+	 * @param float|int $a
+	 * @return string
+	 */
+	function amountFormat($a) {return df_f2($a);}
+
+	/**
+	 * 2017-04-16
 	 * @used-by \Dfe\Robokassa\Charge::pCharge()
 	 * @return string
 	 */
 	function option() {return df_result_sne($this->iia(self::$II_OPTION));}
+
+	/**
+	 * 2017-04-16
+	 * «The format of presentation – dot-delimited digits. For example 123.45.»
+	 * http://docs.robokassa.ru/en/#2504
+	 * «Формат представления — число, разделитель — точка, например: 123.45.»
+	 * http://docs.robokassa.ru/#1188
+	 * @override
+	 * @see \Df\Payment\Method::amountFactor()
+	 * @used-by \Df\Payment\Method::amountFormat()
+	 * @used-by \Df\Payment\Method::amountParse()
+	 * @return int
+	 */
+	protected function amountFactor() {return 1;}
 
 	/**
 	 * 2017-04-10
