8dd5ef737d607bdc6d6bc632429fc379c9ab1db2
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-27 18:30:53 +0300

    1.3.4

--- T/Basic.php
+++ T/Basic.php
@@ -5,21 +5,21 @@ use \Dfe\Robokassa\Settings as S;
 // 2017-04-12
 final class Basic extends TestCase {
 	/** 2017-04-12 */
-	function t01() {echo df_dump([$this->s()->password1(), $this->s()->password2()]);}
+	function t01() {print_r(df_dump([$this->s()->password1(), $this->s()->password2()]));}
 
 	/** 2017-04-12 */
-	function t02() {echo df_json_encode(Options::forCheckout(20000));}
+	function t02() {print_r(df_json_encode(Options::forCheckout(20000)));}
 
 	/** 2017-04-17 */
-	function t03() {echo intval(df_my());}
+	function t03() {print_r(intval(df_my()));}
 
 	/** @test 2017-08-14 */
 	function t04() {
 		$s = dfps($this); /** @var S $s */
 		preg_match($s->v('identification_rules/regex'), 'adasdas111', $matches);
-		echo df_json_encode($matches);
+		print_r(df_json_encode($matches));
 	}
 
 	/** 2017-08-14 */
-	function t05() {echo __('String %1 string %1', 'variable');}
+	function t05() {print_r(__('String %1 string %1', 'variable'));}
 }
