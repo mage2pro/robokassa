31e8f02799d841de347fb8ae4b99eea2ea8b9d49
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-08 02:34:07 +0000

    refactoring

--- Test/Basic.php
+++ Test/Basic.php
@@ -5,21 +5,21 @@ use Dfe\Robokassa\Settings as S;
 # 2017-04-12
 final class Basic extends CaseT {
 	/** 2017-04-12 */
-	function t01() {print_r(df_dump([$this->s()->password1(), $this->s()->password2()]));}
+	function t01():void {print_r(df_dump([$this->s()->password1(), $this->s()->password2()]));}
 
 	/** 2017-04-12 */
-	function t02() {print_r(df_json_encode(Options::forCheckout(20000)));}
+	function t02():void {print_r(df_json_encode(Options::forCheckout(20000)));}
 
 	/** 2017-04-17 */
-	function t03() {print_r(intval(df_my()));}
+	function t03():void {print_r(intval(df_my()));}
 
 	/** 2017-08-14 @test */
-	function t04() {
+	function t04():void {
 		$s = dfps($this); /** @var S $s */
 		preg_match($s->v('identification_rules/regex'), 'adasdas111', $matches);
 		print_r(df_json_encode($matches));
 	}
 
 	/** 2017-08-14 */
-	function t05() {print_r(__('String %1 string %1', 'variable'));}
+	function t05():void {print_r(__('String %1 string %1', 'variable'));}
 }
